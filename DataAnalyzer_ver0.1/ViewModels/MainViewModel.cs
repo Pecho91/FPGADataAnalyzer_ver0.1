9446d66f481816f765f9ff4880a19ee166f322f3
andrija.pecaric@gmail.com <andrija.pecaric@gmail.com>
2024-06-13 20:25:07 +0200

    update

--- DataAnalyzer_ver0.1/ViewModels/MainViewModel.cs
+++ DataAnalyzer_ver0.1/ViewModels/MainViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using DataAnalyzer_ver0._1.Common.Models;
+using DataAnalyzer_ver0._1.RelayCommands;
+using DataAnalyzer_ver0._1.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DataAnalyzer_ver0._1.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        private readonly IDataAnalyzerService _dataAnalyzerService;
+        private readonly IDataProcessorService _processorService;
+        private readonly IDataReaderService _readerService;
+
+        private FPGADataModel _fpgaData;
+        public FPGADataModel FPGAData
+        {
+            get => _fpgaData;
+            set
+            {
+                _fpgaData = value;
+                OnPropertyChanged(nameof(FPGAData));
+            }
+        }
+
+        public ICommand StartDataAcquisitionCommand { get; set; }
+
+        public MainViewModel(IDataAnalyzerService dataAnalyzerService, IDataProcessorService processorService, IDataReaderService readerService)
+        {
+            _dataAnalyzerService = dataAnalyzerService;
+            _processorService = processorService;
+            _readerService = readerService;
+
+            StartDataAcquisitionCommand = new RelayCommand(async () => await StartDataAcquisition());
+           
+        }
+
+        private async Task StartDataAcquisition()
+        {
+
+        }
     }
 }
