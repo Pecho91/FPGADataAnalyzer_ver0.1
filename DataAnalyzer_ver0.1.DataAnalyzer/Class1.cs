8e97399a3c23f7e5c68151b2da6e71dc873997ef
andrija.pecaric@gmail.com <andrija.pecaric@gmail.com>
2024-06-13 20:00:31 +0200

    update

--- DataAnalyzer_ver0.1.DataAnalyzer/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAnalyzer_ver0._1.DataAnalyzer
-{
-    public class Class1
-    {
-
-    }
-}
--- DataAnalyzer_ver0.1.Services/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAnalyzer_ver0._1.Services
-{
-    public class Class1
-    {
-
-    }
-}
